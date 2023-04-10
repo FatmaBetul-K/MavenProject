ca38e1097a239fd3fcc367841bdf22764c10d08b
FatmaBetul-K <fatma__betull@outlook.com>
2023-04-10 22:54:50 +0300

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello features");
+        System.out.println("Github Repo is already published to GitHub");
+        System.out.println("Doing more work");
 
     }
 }
